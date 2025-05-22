18fbe1a7fe5d0060d4dbc7b55091153d84cb1426
Okr <kr.oleg@gmail.com>
2025-05-23 00:00:33 +0300

    v2.6.0 Update from original repo

--- app/3x-ui/update_xray_bin.sh
+++ app/3x-ui/update_xray_bin.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-case $1 in
+case "$(uname -m)" in
     amd64)
         ARCH="64"
         FNAME="amd64"
@@ -28,10 +28,10 @@ esac
 mkdir -p bin
 cd bin
 find . ! -name 'config.json' -type f -exec rm -f {} +
-wget -q "https://github.com/XTLS/Xray-core/releases/download/v25.4.30/Xray-linux-${ARCH}.zip"
-unzip "Xray-linux-${ARCH}.zip"
-rm -f "Xray-linux-${ARCH}.zip" geoip.dat geosite.dat
-mv xray "xray-linux-${FNAME}"
+wget -q "https://github.com/XTLS/Xray-core/releases/download/v25.5.16/Xray-macos-${ARCH}.zip"
+unzip "Xray-macos-${ARCH}.zip"
+rm -f "Xray-macos-${ARCH}.zip" geoip.dat geosite.dat
+mv xray "xray-darwin-${FNAME}"
 wget -q https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 wget -q https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 wget -q -O geoip_IR.dat https://github.com/chocolate4u/Iran-v2ray-rules/releases/latest/download/geoip.dat
